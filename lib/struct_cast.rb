f3d15f28ff9890a8fed262f4831d52c669b0ce3a
Yuji Hanamura <yuji.developer@gmail.com>
2020-09-26 02:27:31 +0900

    Make it simple

--- lib/struct_cast.rb
+++ lib/struct_cast.rb
@@ -28,26 +28,19 @@ module StructCast
   end
   private_constant :CoreExt
 
-  module Method
-    using CoreExt
+  [::Kernel, ::Kernel.singleton_class].each do |klass|
+    refine klass do
+      using CoreExt
 
-    def Struct(args)
-      hash = args.convert_to_hash.symbolize_keys
+      def Struct(args)
+        hash = args.convert_to_hash.symbolize_keys
 
-      if hash.empty?
-        ::Struct.new(nil, keyword_init: true).new
-      else
-        ::Struct.new(*hash.keys, keyword_init: true).new(**hash)
+        if hash.empty?
+          ::Struct.new(nil, keyword_init: true).new
+        else
+          ::Struct.new(*hash.keys, keyword_init: true).new(**hash)
+        end
       end
     end
-  end
-  private_constant :Method
-
-  refine ::Kernel do
-    include Method
-  end
-
-  refine ::Kernel.singleton_class do
-    include Method
   end
 end
