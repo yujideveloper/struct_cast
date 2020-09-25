d47f73fc70accfd3e330026e8f95d185f93e99a9
Yuji Hanamura <yuji.developer@gmail.com>
2020-09-26 00:24:26 +0900

    Make `Struct()` simple by using small refinement modules

--- lib/struct_cast.rb
+++ lib/struct_cast.rb
@@ -3,22 +3,36 @@
 require "struct_cast/version"
 
 module StructCast
-  refine ::Kernel do
-    def Struct(args)
-      m =
-        if args.respond_to? :to_hash
-          :to_hash
-        elsif args.respond_to? :to_h
-          :to_h
+  module CoreExt
+    refine ::Object do
+      def convert_to_hash
+        if self.respond_to? :to_hash
+          self.to_hash
+        elsif self.respond_to? :to_h
+          self.to_h
         else
-          raise ::TypeError, "can't convert #{args.class.name} to Hash"
+          raise ::TypeError, "can't convert #{self.class.name} to Hash"
         end
+      end
+    end
 
-      hash =
-        args.__send__(m).transform_keys do |k|
-          raise ::ArgumentError, "invalid key type for #{__callee__}(): #{k.class.name}" unless k.respond_to? :to_sym
+    refine ::Hash do
+      def symbolize_keys
+        self.transform_keys do |k|
+          raise ::ArgumentError, "can't convert key type to Symbol: #{k.class.name}" unless k.respond_to? :to_sym
+
           k.to_sym
         end
+      end
+    end
+  end
+  private_constant :CoreExt
+
+  refine ::Kernel do
+    using CoreExt
+
+    def Struct(args)
+      hash = args.convert_to_hash.symbolize_keys
 
       ::Struct.new(*hash.keys, keyword_init: true).new(**hash)
     end
